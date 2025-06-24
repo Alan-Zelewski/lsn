940dff9ba5ab6cc36e0e7bd3ee23e7f382459902
Alan Zelewski <alan.zelewski@ug.edu.pl>
2025-06-24 10:35:27 +0200

    Task2 - Implements a utility to find and print pairs of numbers from input that sum to a predefined TARGET value.

---
+++ task-2/src/Task2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Task2 {
+
+    private static final int TARGET = 13;
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String inputString = reader.readLine();
+            if(inputString == null || inputString.trim().isEmpty()) {
+                System.out.println("Empty input");
+                return;
+            }
+            String[] inputArray = inputString.split("\\s+");
+            Map<Integer, Integer> frequencyMap = new HashMap<>();
+            List<int[]> result = new ArrayList<>();
+            for(String number : inputArray) {
+                int value = Integer.parseInt(number);
+                frequencyMap.put(value, frequencyMap.getOrDefault(value, 0) + 1);
+            }
+            List<Integer> uniqueNumbers = new ArrayList<>(frequencyMap.keySet());
+            uniqueNumbers.sort(Integer::compareTo);
+            for(Integer number : uniqueNumbers) {
+                int requiredValue = TARGET - number;
+                if(requiredValue < number) {
+                    continue;
+                }
+
+                if(frequencyMap.containsKey(requiredValue)) {
+                    for(int i = 0; i < frequencyMap.get(number); i++) {
+                        result.add(new int[]{number, requiredValue});
+                    }
+                }
+            }
+
+            result.sort(Comparator.comparingInt((int[] arr) -> arr[0])
+                    .thenComparingInt(arr -> arr[1]));
+
+            for (int[] pair : result) {
+                System.out.println(pair[0] + " " + pair[1]);
+            }
+
+        }catch (IOException | NumberFormatException e) {
+            System.out.println("An exception occurred: " + e);
+        }
+    }
+}
